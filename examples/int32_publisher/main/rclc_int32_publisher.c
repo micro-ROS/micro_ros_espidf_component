22e27a73c768652f632144b7b19a01dc6477f32a
Pablo Garrido <pablogs9@gmail.com>
2020-11-03 07:49:13 +0100

    Update rclc_int32_publisher.c

--- examples/int32_publisher/main/rclc_int32_publisher.c
+++ examples/int32_publisher/main/rclc_int32_publisher.c
@@ -21,7 +21,7 @@ std_msgs__msg__Int32 msg;
 
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 {
-	UNUSED(last_call_time);
+	RCLC_UNUSED(last_call_time);
 	if (timer != NULL) {
 		RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
 		msg.data++;

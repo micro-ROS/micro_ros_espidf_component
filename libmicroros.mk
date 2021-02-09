37813d1225d21181b2007d3ecfc1fbd232673cc7
Pablo Garrido <pablogs9@gmail.com>
2021-02-09 12:40:26 +0100

    Revert branches

--- libmicroros.mk
+++ libmicroros.mk
@@ -48,12 +48,12 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 	rm -rf micro_ros_src; \
 	mkdir micro_ros_src; cd micro_ros_src; \
 	git clone -b foxy https://github.com/eProsima/micro-CDR src/micro-CDR; \
-	git clone -b feature/refactor_external_transports https://github.com/eProsima/Micro-XRCE-DDS-Client src/Micro-XRCE-DDS-Client; \
+	git clone -b foxy https://github.com/eProsima/Micro-XRCE-DDS-Client src/Micro-XRCE-DDS-Client; \
 	git clone -b foxy https://github.com/micro-ROS/rcl src/rcl; \
 	git clone -b master https://github.com/micro-ROS/rclc src/rclc; \
 	git clone -b foxy https://github.com/micro-ROS/rcutils src/rcutils; \
 	git clone -b foxy https://github.com/micro-ROS/micro_ros_msgs src/micro_ros_msgs; \
-	git clone -b feature/refactor_external_transports https://github.com/micro-ROS/rmw-microxrcedds src/rmw-microxrcedds; \
+	git clone -b foxy https://github.com/micro-ROS/rmw-microxrcedds src/rmw-microxrcedds; \
 	git clone -b foxy https://github.com/micro-ROS/rosidl_typesupport src/rosidl_typesupport; \
 	git clone -b foxy https://github.com/micro-ROS/rosidl_typesupport_microxrcedds src/rosidl_typesupport_microxrcedds; \
 	git clone -b master https://github.com/ros2/tinydir_vendor src/tinydir_vendor; \

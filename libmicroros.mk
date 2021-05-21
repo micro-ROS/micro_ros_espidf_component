f7eb967508c59dbe8bf020a7f605c95fcc1523dc
Pablo Garrido <pablogs9@gmail.com>
2021-05-21 09:38:19 +0200

    Enable introspection

--- libmicroros.mk
+++ libmicroros.mk
@@ -67,7 +67,6 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 	git clone -b master https://github.com/ros2/rmw_implementation src/rmw_implementation; \
 	git clone -b master https://github.com/ros2/rcl_logging src/rcl_logging; \
 	git clone -b master https://gitlab.com/micro-ROS/ros_tracing/ros2_tracing src/ros2_tracing; \
-	touch src/rosidl/rosidl_typesupport_introspection_c/COLCON_IGNORE; \
     touch src/rosidl/rosidl_typesupport_introspection_cpp/COLCON_IGNORE; \
     touch src/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE; \
     touch src/rcl_logging/rcl_logging_spdlog/COLCON_IGNORE; \

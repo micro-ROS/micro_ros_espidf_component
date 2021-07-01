e46aa8fcf37bd25dec16ceab46b4b7d4bb192010
Pablo Garrido <pablogs9@gmail.com>
2021-07-01 07:57:28 +0200

    Update libmicroros.mk

--- libmicroros.mk
+++ libmicroros.mk
@@ -50,7 +50,6 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 	mkdir micro_ros_src; cd micro_ros_src; \
 	git clone -b foxy https://github.com/eProsima/micro-CDR src/micro-CDR; \
 	git clone -b foxy https://github.com/eProsima/Micro-XRCE-DDS-Client src/Micro-XRCE-DDS-Client; \
-<<<<<<< HEAD
 	git clone -b galactic https://github.com/micro-ROS/rcl src/rcl; \
 	git clone -b galactic https://github.com/ros2/rclc src/rclc; \
 	git clone -b galactic https://github.com/micro-ROS/rcutils src/rcutils; \
@@ -68,31 +67,11 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 	git clone -b galactic https://github.com/ros2/rmw_implementation src/rmw_implementation; \
 	git clone -b galactic https://github.com/ros2/rcl_logging src/rcl_logging; \
 	git clone -b galactic https://gitlab.com/micro-ROS/ros_tracing/ros2_tracing src/ros2_tracing; \
-	git clone -b galactic https://github.com/micro-ROS/micro_ros_utilities; \
-=======
-	git clone -b master https://github.com/micro-ROS/rcl src/rcl; \
-	git clone -b master https://github.com/ros2/rclc src/rclc; \
-	git clone -b master https://github.com/micro-ROS/rcutils src/rcutils; \
-	git clone -b main https://github.com/micro-ROS/micro_ros_msgs src/micro_ros_msgs; \
-	git clone -b main https://github.com/micro-ROS/rmw-microxrcedds src/rmw-microxrcedds; \
-	git clone -b master https://github.com/micro-ROS/rosidl_typesupport src/rosidl_typesupport; \
-	git clone -b main https://github.com/micro-ROS/rosidl_typesupport_microxrcedds src/rosidl_typesupport_microxrcedds; \
-	git clone -b master https://github.com/ros2/rosidl src/rosidl; \
-	git clone -b master https://github.com/ros2/rmw src/rmw; \
-	git clone -b master https://github.com/ros2/rcl_interfaces src/rcl_interfaces; \
-	git clone -b master https://github.com/ros2/rosidl_defaults src/rosidl_defaults; \
-	git clone -b master https://github.com/ros2/unique_identifier_msgs src/unique_identifier_msgs; \
-	git clone -b master https://github.com/ros2/common_interfaces src/common_interfaces; \
-	git clone -b master https://github.com/ros2/test_interface_files src/test_interface_files; \
-	git clone -b master https://github.com/ros2/rmw_implementation src/rmw_implementation; \
-	git clone -b master https://github.com/ros2/rcl_logging src/rcl_logging; \
-	git clone -b master https://gitlab.com/micro-ROS/ros_tracing/ros2_tracing src/ros2_tracing; \
-	git clone -b main https://github.com/micro-ROS/micro_ros_utilities src/micro_ros_utilities; \
->>>>>>> 5ad1212 (Fix utils path (#73))
-    touch src/rosidl/rosidl_typesupport_introspection_cpp/COLCON_IGNORE; \
-    touch src/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE; \
-    touch src/rcl_logging/rcl_logging_spdlog/COLCON_IGNORE; \
-    touch src/rclc/rclc_examples/COLCON_IGNORE; \
+	git clone -b galactic https://github.com/micro-ROS/micro_ros_utilities src/micro_ros_utilities; \
+	touch src/rosidl/rosidl_typesupport_introspection_cpp/COLCON_IGNORE; \
+	touch src/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE; \
+	touch src/rcl_logging/rcl_logging_spdlog/COLCON_IGNORE; \
+	touch src/rclc/rclc_examples/COLCON_IGNORE; \
 	touch src/rcl/rcl_yaml_param_parser/COLCON_IGNORE; \
 	cp -rf ../extra_packages src/extra_packages || :;
 

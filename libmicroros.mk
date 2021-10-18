b705d703f0193393cf3ecb5008e947bd5fde6477
Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
2021-10-18 15:09:43 +0200

    Set ESP flag and change rtps branch (#99)
    * Set ESP flag and change rtps branch
    
    * Update libmicroros.mk

--- libmicroros.mk
+++ libmicroros.mk
@@ -49,7 +49,7 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 	rm -rf micro_ros_src; \
 	mkdir micro_ros_src; cd micro_ros_src; \
 	if [ "$(MIDDLEWARE)" = "embeddedrtps" ]; then \
-		git clone -b working_esp32 https://github.com/pablogs9/embeddedRTPS src/embeddedRTPS; \
+		git clone -b galactic https://github.com/pablogs9/embeddedRTPS src/embeddedRTPS; \
 		git clone -b galactic https://github.com/micro-ROS/rmw_embeddedrtps src/rmw_embeddedrtps; \
 	else \
 		git clone -b ros2 https://github.com/eProsima/Micro-XRCE-DDS-Client src/Micro-XRCE-DDS-Client; \

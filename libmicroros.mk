8baa7682cad46a40ff19688c7361756ce216cce4
Pablo Garrido <pablogs9@gmail.com>
2021-11-10 11:32:51 +0100

    Add support for ESP-IDF v4.4 (#110)
    * Add support for ESP-IDF v4.4
    
    * Add includes
    
    * Add includes
    
    * Fix atomics
    
    (cherry picked from commit 56ff1752b82533d469dd6f25a0b58919ddf86532)
    
    # Conflicts:
    #	esp32_toolchain.cmake.in
    #	libmicroros.mk

--- libmicroros.mk
+++ libmicroros.mk
@@ -103,7 +103,15 @@ ifeq ($(IDF_TARGET),$(filter $(IDF_TARGET),esp32s2 esp32c3))
 		if [ $(IDF_VERSION_MAJOR) -ge 4 ] && [ $(IDF_VERSION_MINOR) -ge 3 ]; then \
 			$(STRIP) atomic_64bits.c.obj --strip-symbol=__atomic_exchange_8; \
 		fi; \
+<<<<<<< HEAD
 		$(AR) rc -s librcutils.a *.obj; \
+=======
+		if [ $(IDF_VERSION_MAJOR) -ge 4 ] && [ $(IDF_VERSION_MINOR) -ge 4 ]; then \
+			$(X_STRIP) atomic_64bits.c.obj --strip-symbol=__atomic_load_8; \
+			$(X_STRIP) atomic_64bits.c.obj --strip-symbol=__atomic_store_8; \
+		fi; \
+		$(X_AR) rc -s librcutils.a *.obj; \
+>>>>>>> 56ff175 (Add support for ESP-IDF v4.4 (#110))
 		cp -rf librcutils.a  $(UROS_DIR)/install/lib/librcutils.a; \
 		rm -rf $(UROS_DIR)/atomic_workaround; \
 		cd ..; 

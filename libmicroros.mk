micro-ROS/micro_ros_espidf_component

commit 70879ffa3a2fd0ea7a801080c49e3bf37e4451f1

Fix for building after sourcing ROS setup script (#63)
* Remove global ROS variables from environment before build

Unsets `AMENT_PREFIX_PATH` and removes `/opt/ros/$(ROS_DISTRO)/bin`
from `PATH` before building micro_ros_src, in case they've been set
by the ROS setup scripts beforehand.

* Added note to README about running in a clean shell environment

Build can have issues if ROS 2 setup script has been sourced, so made a
note of that here.

libmicroros.mk
@@ -75,6 +75,8 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 
 $(EXTENSIONS_DIR)/micro_ros_src/install: $(EXTENSIONS_DIR)/esp32_toolchain.cmake $(EXTENSIONS_DIR)/micro_ros_dev/install $(EXTENSIONS_DIR)/micro_ros_src/src
 	cd $(UROS_DIR); \
+	unset AMENT_PREFIX_PATH; \
+	PATH=$(subst /opt/ros/$(ROS_DISTRO)/bin,,$(PATH)); \
 	. ../micro_ros_dev/install/local_setup.sh; \
 	colcon build \
 		--merge-install \

commit ca53966a25ea27e1bf7399890a70b0eeb4c1f72a

Fixed build when PATH contains spaces
Fixes issue introduced in #63 - if PATH contains spaces (or special characters) when this line of the Makefile runs, it will corrupt the PATH variable and the build will fail.
Putting quotes around the result of the string substitution fixes this.

libmicroros.mk
@@ -85,7 +85,7 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 $(EXTENSIONS_DIR)/micro_ros_src/install: $(EXTENSIONS_DIR)/esp32_toolchain.cmake $(EXTENSIONS_DIR)/micro_ros_dev/install $(EXTENSIONS_DIR)/micro_ros_src/src
 	cd $(UROS_DIR); \
 	unset AMENT_PREFIX_PATH; \
-	PATH=$(subst /opt/ros/$(ROS_DISTRO)/bin,,$(PATH)); \
+	PATH="$(subst /opt/ros/$(ROS_DISTRO)/bin,,$(PATH))"; \
 	. ../micro_ros_dev/install/local_setup.sh; \
 	colcon build \
 		--merge-install \
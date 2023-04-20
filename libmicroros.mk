2f20fa7630ef59aed27479a1fd98b0a4af7e70b6
Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
2023-04-20 10:01:15 +0200

    Fix rolling sources (#185)
    * Fix rolling sources
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    
    * Remove type description
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    
    * Test with debug build
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    
    * Test with debug build
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    
    * Test with removed debug
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    
    * Test build with NDEBUG
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    
    * Test with RelWithDebInfo
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    
    * Test with CONFIG_COMPILER_OPTIMIZATION_ASSERTIONS_DISABLE
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    
    * Add assert definition
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    
    * Override assert on compiler flags
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    
    * Add assert override
    
    ---------
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    Co-authored-by: Pablo Garrido <pablogs9@gmail.com>

--- libmicroros.mk
+++ libmicroros.mk
@@ -37,12 +37,12 @@ $(EXTENSIONS_DIR)/esp32_toolchain.cmake: $(EXTENSIONS_DIR)/esp32_toolchain.cmake
 $(EXTENSIONS_DIR)/micro_ros_dev/install:
 	rm -rf micro_ros_dev; \
 	mkdir micro_ros_dev; cd micro_ros_dev; \
-	git clone -b master https://github.com/ament/ament_cmake src/ament_cmake; \
-	git clone -b master https://github.com/ament/ament_lint src/ament_lint; \
-	git clone -b master https://github.com/ament/ament_package src/ament_package; \
-	git clone -b ros2 https://github.com/ament/googletest src/googletest; \
-	git clone -b master https://github.com/ros2/ament_cmake_ros src/ament_cmake_ros; \
-	git clone -b master https://github.com/ament/ament_index src/ament_index; \
+	git clone -b rolling https://github.com/ament/ament_cmake src/ament_cmake; \
+	git clone -b rolling https://github.com/ament/ament_lint src/ament_lint; \
+	git clone -b rolling https://github.com/ament/ament_package src/ament_package; \
+	git clone -b rolling https://github.com/ament/googletest src/googletest; \
+	git clone -b rolling https://github.com/ros2/ament_cmake_ros src/ament_cmake_ros; \
+	git clone -b rolling https://github.com/ament/ament_index src/ament_index; \
 	colcon build --cmake-args -DBUILD_TESTING=OFF -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=gcc;
 
 $(EXTENSIONS_DIR)/micro_ros_src/src:
@@ -62,17 +62,18 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 	git clone -b main https://github.com/micro-ROS/micro_ros_msgs src/micro_ros_msgs; \
 	git clone -b master https://github.com/micro-ROS/rosidl_typesupport src/rosidl_typesupport; \
 	git clone -b main https://github.com/micro-ROS/rosidl_typesupport_microxrcedds src/rosidl_typesupport_microxrcedds; \
-	git clone -b master https://github.com/ros2/rosidl src/rosidl; \
-	git clone -b master https://github.com/ros2/rmw src/rmw; \
-	git clone -b master https://github.com/ros2/rcl_interfaces src/rcl_interfaces; \
-	git clone -b master https://github.com/ros2/rosidl_defaults src/rosidl_defaults; \
-	git clone -b master https://github.com/ros2/unique_identifier_msgs src/unique_identifier_msgs; \
-	git clone -b master https://github.com/ros2/common_interfaces src/common_interfaces; \
-	git clone -b master https://github.com/ros2/example_interfaces src/example_interfaces; \
-	git clone -b master https://github.com/ros2/test_interface_files src/test_interface_files; \
-	git clone -b master https://github.com/ros2/rmw_implementation src/rmw_implementation; \
-	git clone -b master https://github.com/ros2/rcl_logging src/rcl_logging; \
-	git clone -b master https://github.com/ros2/ros2_tracing src/ros2_tracing; \
+	git clone -b rolling https://github.com/ros2/rosidl src/rosidl; \
+	git clone -b rolling https://github.com/ros2/rosidl_dynamic_typesupport src/rosidl_dynamic_typesupport; \
+	git clone -b rolling https://github.com/ros2/rmw src/rmw; \
+	git clone -b rolling https://github.com/ros2/rcl_interfaces src/rcl_interfaces; \
+	git clone -b rolling https://github.com/ros2/rosidl_defaults src/rosidl_defaults; \
+	git clone -b rolling https://github.com/ros2/unique_identifier_msgs src/unique_identifier_msgs; \
+	git clone -b rolling https://github.com/ros2/common_interfaces src/common_interfaces; \
+	git clone -b rolling https://github.com/ros2/example_interfaces src/example_interfaces; \
+	git clone -b rolling https://github.com/ros2/test_interface_files src/test_interface_files; \
+	git clone -b rolling https://github.com/ros2/rmw_implementation src/rmw_implementation; \
+	git clone -b rolling https://github.com/ros2/rcl_logging src/rcl_logging; \
+	git clone -b rolling https://github.com/ros2/ros2_tracing src/ros2_tracing; \
 	git clone -b main https://github.com/micro-ROS/micro_ros_utilities src/micro_ros_utilities; \
 	git clone -b rolling https://github.com/ros2/rosidl_core src/rosidl_core; \
     touch src/rosidl/rosidl_typesupport_introspection_cpp/COLCON_IGNORE; \

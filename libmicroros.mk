f8422574cb43b97d2fbdb32b316bb9b1bcc4d56a
acuadros95 <acuadros1995@gmail.com>
2023-05-11 12:12:23 +0200

    Replace repo with micro-ROS fork
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>

--- libmicroros.mk
+++ libmicroros.mk
@@ -63,7 +63,7 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 	git clone -b master https://github.com/micro-ROS/rosidl_typesupport src/rosidl_typesupport; \
 	git clone -b main https://github.com/micro-ROS/rosidl_typesupport_microxrcedds src/rosidl_typesupport_microxrcedds; \
 	git clone -b rolling https://github.com/ros2/rosidl src/rosidl; \
-	git clone -b rolling https://github.com/ros2/rosidl_dynamic_typesupport src/rosidl_dynamic_typesupport; \
+	git clone -b rolling https://github.com/micro-ROS/rosidl_dynamic_typesupport src/rosidl_dynamic_typesupport; \
 	git clone -b rolling https://github.com/ros2/rmw src/rmw; \
 	git clone -b rolling https://github.com/ros2/rcl_interfaces src/rcl_interfaces; \
 	git clone -b rolling https://github.com/ros2/rosidl_defaults src/rosidl_defaults; \

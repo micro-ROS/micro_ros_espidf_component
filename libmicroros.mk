25bedf49de466423dd6f08f26263e19a9388c414
Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
2023-06-08 12:07:47 +0200

    Update rolling branches (#197)
    * Update rolling branches
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    
    * Update rclc to rolling
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    
    ---------
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>

--- libmicroros.mk
+++ libmicroros.mk
@@ -53,15 +53,15 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 		git clone -b main https://github.com/micro-ROS/rmw_embeddedrtps src/rmw_embeddedrtps; \
 	else \
 		git clone -b ros2 https://github.com/eProsima/Micro-XRCE-DDS-Client src/Micro-XRCE-DDS-Client; \
-		git clone -b main https://github.com/micro-ROS/rmw_microxrcedds src/rmw_microxrcedds; \
+		git clone -b rolling https://github.com/micro-ROS/rmw_microxrcedds src/rmw_microxrcedds; \
 	fi; \
 	git clone -b ros2 https://github.com/eProsima/micro-CDR src/micro-CDR; \
-	git clone -b master https://github.com/micro-ROS/rcl src/rcl; \
-	git clone -b master https://github.com/ros2/rclc src/rclc; \
-	git clone -b master https://github.com/micro-ROS/rcutils src/rcutils; \
-	git clone -b main https://github.com/micro-ROS/micro_ros_msgs src/micro_ros_msgs; \
-	git clone -b master https://github.com/micro-ROS/rosidl_typesupport src/rosidl_typesupport; \
-	git clone -b main https://github.com/micro-ROS/rosidl_typesupport_microxrcedds src/rosidl_typesupport_microxrcedds; \
+	git clone -b rolling https://github.com/micro-ROS/rcl src/rcl; \
+	git clone -b rolling https://github.com/ros2/rclc src/rclc; \
+	git clone -b rolling https://github.com/micro-ROS/rcutils src/rcutils; \
+	git clone -b rolling https://github.com/micro-ROS/micro_ros_msgs src/micro_ros_msgs; \
+	git clone -b rolling https://github.com/micro-ROS/rosidl_typesupport src/rosidl_typesupport; \
+	git clone -b rolling https://github.com/micro-ROS/rosidl_typesupport_microxrcedds src/rosidl_typesupport_microxrcedds; \
 	git clone -b rolling https://github.com/ros2/rosidl src/rosidl; \
 	git clone -b rolling https://github.com/micro-ROS/rosidl_dynamic_typesupport src/rosidl_dynamic_typesupport; \
 	git clone -b rolling https://github.com/ros2/rmw src/rmw; \
@@ -74,7 +74,7 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 	git clone -b rolling https://github.com/ros2/rmw_implementation src/rmw_implementation; \
 	git clone -b rolling https://github.com/ros2/rcl_logging src/rcl_logging; \
 	git clone -b rolling https://github.com/ros2/ros2_tracing src/ros2_tracing; \
-	git clone -b main https://github.com/micro-ROS/micro_ros_utilities src/micro_ros_utilities; \
+	git clone -b rolling https://github.com/micro-ROS/micro_ros_utilities src/micro_ros_utilities; \
 	git clone -b rolling https://github.com/ros2/rosidl_core src/rosidl_core; \
     touch src/rosidl/rosidl_typesupport_introspection_cpp/COLCON_IGNORE; \
     touch src/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE; \

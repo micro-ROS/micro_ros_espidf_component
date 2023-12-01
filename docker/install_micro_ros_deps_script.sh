7c4fb4c925b27f101acc30a95054f49932d2e200
Pablo Garrido <pablogs9@gmail.com>
2023-12-01 08:11:50 +0100

    Fix empy dep (#213)
    (cherry picked from commit dd5108214f747780ac401ab50a2b6ae97f1480fc)

--- docker/install_micro_ros_deps_script.sh
+++ docker/install_micro_ros_deps_script.sh
@@ -5,6 +5,6 @@ set -eu
 sudo apt update -q
 sudo apt install -yq python3-pip
 source $IDF_PATH/export.sh
-pip3 install catkin_pkg lark-parser empy colcon-common-extensions importlib-resources
+pip3 install catkin_pkg lark-parser colcon-common-extensions importlib-resources
 
 set +u

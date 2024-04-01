7d9dec9f51efdac9c6c73686fd4764a996113b41
Pablo Garrido <pablogs9@gmail.com>
2024-04-01 14:35:50 +0200

    Fix

--- include_override/FreeRTOS.h
+++ include_override/FreeRTOS.h
@@ -1,6 +1,2 @@
-// Just for micro-ROS compatibility remove ESP_STATIC_ASSERT from micro-ROS build
-#undef ESP_STATIC_ASSERT
-#define ESP_STATIC_ASSERT(...) ((void)0);
-
 // Include proper path to FreeRTOS header file
 #include "freertos/FreeRTOS.h"

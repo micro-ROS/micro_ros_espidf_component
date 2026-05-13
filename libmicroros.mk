5b12d064888350532a0e365ffae00c950d3c4861
Adrian <67166322+Caedael@users.noreply.github.com>
2026-05-13 13:22:31 +0200

    Change cp command to use -rfL option (#335)
    Resolve symlinks from the `extra_packages` directory
    
    (cherry picked from commit 2f73852d03348d8078148bb729a26a31d291e2f6)

--- libmicroros.mk
+++ libmicroros.mk
@@ -85,7 +85,7 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 	touch src/rcl/rcl_yaml_param_parser/COLCON_IGNORE; \
 	touch src/ros2_tracing/test_tracetools/COLCON_IGNORE; \
 	touch src/ros2_tracing/lttngpy/COLCON_IGNORE; \
-	cp -rf $(EXTRA_ROS_PACKAGES) src/extra_packages || :; \
+	cp -rfL $(EXTRA_ROS_PACKAGES) src/extra_packages || :; \
 	test -f src/extra_packages/extra_packages.repos && cd src/extra_packages && vcs import --input extra_packages.repos || :;
 
 
